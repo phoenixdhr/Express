53797a29f51f00c6f7feebd868a3955be6475291
danyr <danyr@DESKTOP-5DKEN75>
2023-04-09 13:21:41 -0500

    proyecto terminado, es lo mismo que el archivo ubicado en Clases Express-24V2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const cors =require("cors")
 const {
   logErrors,
   errorHandler,
   boomErrorHandle,
 } = require('./middelware/errorHandler');
 
-const cors =require("cors")
 
 const routerApi = require('./router/indexRouter');
 //console.clear();
@@ -13,32 +13,39 @@ const routerApi = require('./router/indexRouter');
 const app = express();
 const port = 3000;
 
-const whitelist = ["http://localhost:5500", "http://localhost:9000","http://localhost:5500/front.html"]
 
-const options ={
-                  origin: (origin,callback)=>{
-                    if (whitelist.includes(origin)) {
-                      callback(null,true)
-                    } else {
-                      callback(new Error("acceso no permitido"))
-                    }
-                  }
-              }
-
-app.use(cors(options))
 
 //midelware nativo de express para recibir informacion en json
 app.use(express.json());
 
-app.listen(port, () => {
-  console.log('Listen Activo!');
-});
+// const whitelist = ["http://localhost:5500"]
+// const options ={
+//                   origin: (origin, callback)=>{
+//                     if (whitelist.includes(origin)) {
+//                       callback(null,true)
+//                     } else {
+//                       callback(new Error("acceso no permitido"))
+//                     }
+//                   }
+//               }
+
+app.use(cors({
+  origin: ["http://localhost:5501"]
+}))
+
+
 
 //Rutas que manejan solicitudes
 routerApi(app);
 
+
+
 app.use(logErrors);
 app.use(boomErrorHandle);
 app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Listen Activo!');
+});
 
 //http://localhost:3000/apiV1/productos
